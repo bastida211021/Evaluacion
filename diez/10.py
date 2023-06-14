005ff90d302da3cd4b6ad2e25da7defa7dfd65d5
bastida211021 <diegobasutida@gmail.com>
2023-06-13 22:14:55 -0600

    actualización

--- diez/10.py
+++ diez/10.py
@@ -13,19 +13,34 @@ en el segundo objeto de la lista de “sectors” indicar el parámetro que no c
 el tipo de dato adecuado  e indicar además la posición del objeto en dicha lista.
 Recomendación. Hacer uso de Excepciones.
 """
-import json
-ruta="diez/response.json"
-def cargardatos(ruta):
+import json #para poder importar el json como diccionario
+rutaoriginal="diez/response.json"#ubicación del json original
+rutasecundario="diez/response2.json"#ubicacion del json secundario
+
+def cargarchivo(ruta):#función para importar el json, recibe la ubicación del archivo, retorna el diccionario importado
     with open(ruta) as contenido:
         cursos=json.load(contenido)
-    #print(cursos)
     contenido.close()
     return cursos
-def analiza_tipos_de_dato(contenidoarchivo):
-    listatipodedato=[]
-    for elemento in contenidoarchivo:
-        listatipodedato.append(type(contenidoarchivo[elemento]))
-    print(listatipodedato)
 
-contenidoarchivo=cargardatos(ruta)
-analiza_tipos_de_dato(contenidoarchivo)
+def compararchivos(jsonoriginal,jsonsecundario,ubi=""):#función para comparar los archivos json
+    #en caso de recibir una variable
+    try:
+        if type(jsonoriginal)!=type(jsonsecundario):
+            raise TypeError
+    except TypeError:
+            print(("El campo: "+ubi+" debe ser "+str(type(jsonoriginal))+" no "+str(type(jsonsecundario))))
+    #en caso de recibir una lista
+    if (type(jsonoriginal)== list):
+        for index, item1 in enumerate(jsonoriginal):
+            ubirelativa = f"{ubi}[{index}]"
+            item2 = jsonsecundario[index]
+            compararchivos(item1, item2,ubirelativa)
+    #en caso de recibir un diccionario
+    if(type(jsonoriginal)== dict):
+        for key in jsonoriginal:
+            ubirelativa= ubi + '/' + key if ubi else key
+            compararchivos(jsonoriginal[key], jsonsecundario[key],ubirelativa)
+jsonoriginal=cargarchivo(rutaoriginal)#carga del archivo base
+jsonsecundario=cargarchivo(rutasecundario)#carga del archivo a comparar
+compararchivos(jsonoriginal,jsonsecundario,"")
